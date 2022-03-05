43d1d5081277315df0c4d13eee8eb92b3cdcee28
d.kanai <daiki.kanai31@gmail.com>
2022-03-05 18:18:10 +0900

    first commit

---
+++ ios-practice/ios_practiceApp.swift
@@ -0,0 +1,20 @@
+//
+//  ios_practiceApp.swift
+//  ios-practice
+//
+//  Created by daiki kanai on 2022/03/05.
+//
+
+import SwiftUI
+
+@main
+struct ios_practiceApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
