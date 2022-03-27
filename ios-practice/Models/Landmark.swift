5a2f3625158285b363776519399a0ec9dfb7011c
d.kanai <contact@dkanai.com>
2022-03-27 23:41:21 +0900

    list view

--- ios-practice/Models/Landmark.swift
+++ ios-practice/Models/Landmark.swift
@@ -2,7 +2,7 @@ import Foundation
 import CoreLocation
 import SwiftUI
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
