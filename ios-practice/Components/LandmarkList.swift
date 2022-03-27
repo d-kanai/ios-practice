d96ba5fc512466e79b59f36b6b2e3698f9df06de
d.kanai <contact@dkanai.com>
2022-03-28 07:48:49 +0900

    computed toggle

--- ios-practice/Components/LandmarkList.swift
+++ ios-practice/Components/LandmarkList.swift
@@ -1,13 +1,26 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
     var body: some View {
         NavigationView {
-            List(landmarks, id: \.id) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }.navigationTitle("Landmarks")
         }
