17858bcd6eb2a0f85170ab192891352ad92174a3
d.kanai <contact@dkanai.com>
2022-03-27 23:15:30 +0900

    combine view

---
+++ ios-practice/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  ios-practice
+//
+//  Created by daiki kanai on 2022/03/27.
+//
+
+import SwiftUI
+
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
--- ios-practice/ContentView.swift
+++ ios-practice/ContentView.swift
@@ -12,90 +12,22 @@ struct ContentView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            MapView().frame(height: 300)
-            Text("Hello, World!").font(.title).foregroundColor(Color.orange)
-            HStack {
-                Text("sub title").font(.subheadline).foregroundColor(Color.secondary)
+            MapView().ignoresSafeArea(edges: .top) .frame(height: 300)
+            VStack(alignment: .leading){
+                CircleImage().offset(x: 60, y: -130).padding(.bottom, -130)
+                Text("Hello, World!").font(.title).foregroundColor(Color.orange)
+                HStack {
+                    Text("sub title").font(.subheadline).foregroundColor(Color.secondary)
+                    Spacer()
+                    Text("d.kanai").font(.subheadline).foregroundColor(Color.secondary)
+                }
+                Divider()
+                Text("About").font(.title2)
+                Text("Description here")
                 Spacer()
-                Text("d.kanai").font(.subheadline).foregroundColor(Color.secondary)
-            }
-            Divider()
-            Text("About").font(.title2)
-            Text("Description here")
-        }.padding()
+            }.padding()
+        }
     }
-//    @Environment(\.managedObjectContext) private var viewContext
-//
-//    @FetchRequest(
-//        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-//        animation: .default)
-//    private var items: FetchedResults<Item>
-//
-//    var body: some View {
-//        NavigationView {
-//            List {
-//                ForEach(items) { item in
-//                    NavigationLink {
-//                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-//                    } label: {
-//                        Text(item.timestamp!, formatter: itemFormatter)
-//                    }
-//                }
-//                .onDelete(perform: deleteItems)
-//            }
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    EditButton()
-//                }
-//                ToolbarItem {
-//                    Button(action: addItem) {
-//                        Label("Add Item", systemImage: "plus")
-//                    }
-//                }
-//            }
-//            Text("Select an item")
-//        }
-//    }
-//
-//    private func addItem() {
-//        withAnimation {
-//            let newItem = Item(context: viewContext)
-//            newItem.timestamp = Date()
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-//
-//    private func deleteItems(offsets: IndexSet) {
-//        withAnimation {
-//            offsets.map { items[$0] }.forEach(viewContext.delete)
-//
-//            do {
-//                try viewContext.save()
-//            } catch {
-//                // Replace this implementation with code to handle the error appropriately.
-//                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-//                let nsError = error as NSError
-//                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-//            }
-//        }
-//    }
-//}
-//
-//private let itemFormatter: DateFormatter = {
-//    let formatter = DateFormatter()
-//    formatter.dateStyle = .short
-//    formatter.timeStyle = .medium
-//    return formatter
-//}()
-//
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -103,5 +35,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
-
-
